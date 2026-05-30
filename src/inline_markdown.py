b7cce97a056506f5e3f77d20a2dca3f9c0ee04d5
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2026-05-30 19:43:23 +0600

    implement split_nodes_delimiter function to parse inline markdown styles

---
+++ src/inline_markdown.py
@@ -0,0 +1,32 @@
+from src.text_node import TextNode, TextType
+
+
+def split_nodes_delimiter(
+    nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
+    new_nodes = []
+
+    for n in nodes:
+        # only split nodes in plain text
+        if n.text_type != TextType.TEXT:
+            new_nodes.append(n)
+            continue
+
+        parts = n.text.split(delimiter)
+
+        if len(parts) % 2 == 0:
+            raise ValueError(
+                f"Invalid markdown syntax: Delimiter '{delimiter}' was never closed"
+            )
+
+        for i in range(len(parts)):
+            if parts[i] == "":
+                continue
+            if i % 2 == 0:
+                # plain text
+                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                # formatted text
+                new_nodes.append(TextNode(parts[i], text_type))
+
+    return new_nodes
